c19d71b39e8c297733255b8844d36663b42c8cae
sbx_user1051 <sbx_user1051@169.254.54.239>
2025-02-17 16:47:13 +0000

    Mon Feb 17 16:47:13 UTC 2025

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,3 +4,10 @@ from solutions.SUM import sum_solution
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum_invalid(self):
+        try:
+            sum_solution.compute(-1, 2)
+        except ValueError as e:
+            assert str(e) == "x and y should be between 0 and 100"
+
