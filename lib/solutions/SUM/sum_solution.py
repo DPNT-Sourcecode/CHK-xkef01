8fce43337dffdc331415c64a410617598d5eaba2
sbx_user1051 <sbx_user1051@169.254.54.239>
2025-02-17 16:44:17 +0000

    Mon Feb 17 16:44:17 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,6 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    if x<0 or y<0 or x>100 or y>100:
+        raise ValueError("x and y should be between 0 and 100")
+    return x+y
+
