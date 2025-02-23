c5a4d53bec8e66800ca4f6acf8f9a99c0ac2500e
sbx_user1051 <sbx_user1051@169.254.96.165>
2025-02-23 13:10:26 +0000

    Sun Feb 23 13:10:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,7 +11,7 @@ def checkout(skus):
         'B': {'price': 30, 'offer': [{'quantity': 2, 'offer_price': 45}]},
         'C': {'price': 20, 'offer': []},
         'D': {'price': 15, 'offer': []},
-        'E': {'price': 40, 'offer': [{'quantity': 2, 'free_item': 'B'}]}
+        'E': {'price': 40, 'offer': []}#{'quantity': 2, 'free_item': 'B'}
     }
 
     free_items = {
@@ -24,10 +24,12 @@ def checkout(skus):
         
     # Count the number of each item
     item_count = count_items(skus)
+    free_item_count = get_free_item_count(item_count, free_items)
+    item_count_minus_free_items = {item: item_count[item] - free_item_count.get(item, 0) for item in item_count}
 
     # Calculate the total price based on the price table and offers
     total_price = 0  # Initialize total_price to 0
-    for item, count in item_count.items():
+    for item, count in item_count_minus_free_items.items():
         if item in price_table:
             price = price_table[item]['price']
             offers = price_table[item]['offer']
@@ -42,17 +44,8 @@ def checkout(skus):
 
                         remaining_count = count % best_offer['quantity']
                         print(f"item:{item},remaining_count: {remaining_count}")
-                        if item != 'E':
 
-                            total_price += offer_count * best_offer['offer_price'] #+ remaining_count * price
-                        else:
-                            total_price += best_offer['quantity'] * price #+ remaining_count * price
-                        #this is missing an important part of the logic, item B is only free if item E is listed first in the input
-
-                        if item == 'E':
-                            free_item = best_offer['free_item']
-                            free_item_count = min(offer_count, item_count.get(free_item, 0))
-                            total_price -= free_item_count * price_table[free_item]['price']
+                        total_price += offer_count * best_offer['offer_price'] 
                         count = remaining_count
                     else:
                         total_price += count * price
@@ -79,27 +72,15 @@ def get_free_item_count(item_count, free_items):
             associated_item_count = item_count[free_items[free_item]['associated_item']]
             free_item_count[free_item] = associated_item_count // free_items[free_item]['quantity']
     return free_item_count
-            
-   
-# this function returns the best offer for a given quantity, it caluculates the the total price of the item given each offer and returns the lowest total price
-# def get_best_total_offer(offers, count):
-#     offer_total = []
-#     #best_offer = None
-#     for offer in offers:
-#         offer_total_price = 0
-#         if count >= offer['quantity']:
-#             # the best offer is the one with the highest quantity
-#             if not best_offer or offer['quantity'] > best_offer['quantity']:
-#                 best_offer = offer
-#     return best_offer
+
 
 def get_best_offer(offers, count):
     best_offer = None
     for offer in offers:
         if count >= offer['quantity']:
-            # the best offer is the one with the highest quantity
             if not best_offer or offer['quantity'] > best_offer['quantity']:
                 best_offer = offer
     return best_offer
+
 
 
